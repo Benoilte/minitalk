bf5c36795ba158cb747a6765ee48c72f63a24b22
Bebrandt <benoit.07@hotmail.com>
2024-03-05 23:09:13 +0100

    handle signal sigusr1 and sigusr2 + convert string into signal pass to server

--- srcs/client.c
+++ srcs/client.c
@@ -6,7 +6,7 @@
 /*   By: bebrandt <bebrandt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 13:25:46 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/03/05 18:43:32 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/03/05 22:39:38 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,33 @@
 int	main(int argc, char **argv)
 {
 	pid_t	pid;
+	int		i;
+	int		bit;
+	char	*str;
 
 	if (argc == 3)
 	{
+		str = argv[2];
 		pid = ft_atoi(argv[1]);
 		if (pid <= 0)
 			exit (0);
-		ft_printf("kill function: %d\n", kill(ft_atoi(argv[1]), SIGINT));
+		i = 0;
+		while (str[i])
+		{
+			ft_printf("%c\n", str[i]);
+			bit = 0;
+			while (bit < 8)
+			{
+				ft_printf("bit[%d]: %d\n", bit, (str[i] >> (7 - bit)) & MASK);
+				if (((str[i] >> (7 - bit)) & MASK) == 0)
+					kill(ft_atoi(argv[1]), SIGUSR1);
+				else 
+					kill(ft_atoi(argv[1]), SIGUSR2);
+				bit++;
+				usleep(500);
+			}
+			i++;
+		}
 	}
 	else
 		ft_printf("Error: Wrong number of argument\n");
--- srcs/server.c
+++ srcs/server.c
@@ -6,18 +6,52 @@
 /*   By: bebrandt <bebrandt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 13:26:23 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/03/05 18:43:44 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/03/05 23:00:27 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
+t_code code;
+
 int	main(void)
 {
+	code.n = 0;
+	code.c = FLAG_0;
+	set_signal_action();
 	ft_printf("PID: %d\n", getpid());
 	while (1)
 	{
-		sleep(1);
+		pause();
 	}
 	return (0);
+}
+
+void	set_signal_action(void)
+{
+	struct sigaction	act;
+
+	act.sa_flags = 0;
+	act.sa_mask = 0;
+	act.sa_handler = 0;
+	act.sa_sigaction = 0;
+	act.sa_handler = &sig_handler;
+	sigaction(SIGUSR1, &act, NULL);
+	sigaction(SIGUSR2, &act, NULL);
+	sigaction(SIGINT, &act, NULL);
+}
+
+void	sig_handler(int signum)
+{
+	code.n++;
+	if (signum == SIGUSR1)
+		code.c = code.c << 1;
+	else if (signum == SIGUSR2)
+		code.c = (code.c << 1) | FLAG_1;
+	if (code.n == 8)
+	{
+		ft_printf("%c", code.c);
+		code.n = 0;
+		code.c = FLAG_0;
+	}
 }
